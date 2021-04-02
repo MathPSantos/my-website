9af94b5d6fb563bb3218e65a64341a09c8eb53fa
MathPSantos <matheus.psantos2016@gmail.com>
2021-04-02 19:27:54 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 import Document, {
   DocumentInitialProps,
   DocumentContext,
   Html,
   Head,
   Main,
-  NextScript
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        })
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -30,10 +31,10 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
-      }
+        ),
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -42,12 +43,38 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/image/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/image/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/image/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/image/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/image/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
